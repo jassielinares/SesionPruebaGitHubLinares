d9f594635d0f617216e256974dc237a66a516405
linares <jassiel.boxer09@gmail.com>
2021-11-02 22:31:44 -0600

    Prueba 1

---
+++ src/vista/PruebaGitHub.java
@@ -0,0 +1,12 @@
+
+package vista;
+
+import javax.swing.JOptionPane;
+
+public class PruebaGitHub {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Prueba Git Hub\nLinares\nIngenieria de software");
+    }
+    
+}
